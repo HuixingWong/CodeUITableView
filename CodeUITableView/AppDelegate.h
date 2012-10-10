a5c49e40e3834d17e0196dcb690c5f8ea8319dd6
wu bei <wubei@wumatoMac-mini.local>
2012-10-10 20:22:25 +0800

    Initial Commit

---
+++ CodeUITableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CodeUITableView
+//
+//  Created by wu bei on 12-10-10.
+//  Copyright (c) 2012年 wu bei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CodeUITableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CodeUITableView
+//
+//  Created by wu bei on 12-10-10.
+//  Copyright (c) 2012年 wu bei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
